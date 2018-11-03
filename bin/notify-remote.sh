cee6306438c306113e594aafe509546a6d56efb6
Dmitry Krutikov <mitya@openmailbox.org>
2018-11-04 01:22:02 +0300

    newsbeuter notifies

--- bin/notify-remote.sh
+++ bin/notify-remote.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e
 
 read line
+URGENCY="$line"
+read line
 TITLE="$line"
 read line
 MESSAGE="$line"
 read line
 DELIM="$line"
 
+[[ $URGENCY =~ ^(low|normal|critical)$ ]] || exit 1;
+
 if [ "$DELIM" == "" ] && [ "$TITLE" != "" ]; then
-  notify-send -u normal -t 0 -- "$TITLE" "$MESSAGE"
+  notify-send -u $URGENCY -- "$TITLE" "$MESSAGE"
 fi
 
 exit 0
